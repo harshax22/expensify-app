81a4b5c6a0965abf7039dbf53a903d0d17be63a5
harshax22 <h22tswift13@gmail.com>
2019-10-08 18:03:14 +0530

    Setup production webpack build

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 import toJson from 'enzyme-to-json';
+import moment from 'moment';
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -26,4 +27,47 @@ beforeEach(() => {
 
 test('should render ExpenseListFilters correctly', () => {
   expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test('should render ExpenseListFilters with alt data correctly', () => {
+  wrapper.setProps({
+    filters: altFilters
+  })
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test('should handle text change', () => {
+  const value = 'rent'
+  wrapper.find('input').simulate('change', {
+    target: { value }
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+  const value = 'date'
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  })
+  expect(sortByDate).toHaveBeenCalled();
+});
+test('should sort by amount', () => {
+  const value = 'amount'
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  })
+  expect(sortByAmount).toHaveBeenCalled();
+});
+test('should handle date changes', () => {
+  const startDate = moment(0).add(4, 'years');
+  const endDate = moment(0).add(8, 'years');
+  wrapper.find(withStyles(SingleDatePicker)).prop('onDatesChange')({ startDate, endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+test('should handle date focus changes', () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('withStyles(SingleDatePicker)').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
